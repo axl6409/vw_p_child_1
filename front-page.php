a40010813dcdb83fe4311800a8c200c440191f61
axl6409 <alexandre.celier64@gmail.com>
2019-08-19 17:01:18 +0200

    First Commit

--- front-page.php
+++ front-page.php
@@ -22,7 +22,7 @@ get_header(); ?>
 		if ( have_posts() ) :
 			while ( have_posts() ) :
 				the_post();
-				get_template_part( 'template-parts/page/content', 'front-page' );
+				get_template_part( 'template-parts/page/content', 'home-page' );
 			endwhile;
 		else :
 			get_template_part( 'template-parts/post/content', 'none' );
--- template-parts/page/content-home-page.php
+++ template-parts/page/content-home-page.php
@@ -10,13 +10,71 @@
  * @version 1.0
  */
 
+?>
 
-get_header(); ?>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
-		
-		
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'twentyseventeen-panel ' ); ?> >
 
-	</main>
-</div>
+	<?php
+	if ( has_post_thumbnail() ) :
+		$thumbnail = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'twentyseventeen-featured-image' );
+
+		// Calculate aspect ratio: h / w * 100%.
+		$ratio = $thumbnail[2] / $thumbnail[1] * 100;
+		?>
+
+		<div class="panel-image" style="background-image: url(<?php echo esc_url( $thumbnail[0] ); ?>);">
+			<div class="panel-image-prop" style="padding-top: <?php echo esc_attr( $ratio ); ?>%"></div>
+		</div><!-- .panel-image -->
+
+	<?php endif; ?>
+
+	<div class="panel-content">
+		<div class="wrap">
+
+
+			<header class="entry-header">
+				<?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
+
+				<?php twentyseventeen_edit_link( get_the_ID() ); ?>
+
+			</header><!-- .entry-header -->
+
+			<div class="entry-content">
+				<?php
+					/* translators: %s: Name of current post */
+					the_content(
+						sprintf(
+							__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
+							get_the_title()
+						)
+					);
+					?>
+
+					<section>
+						
+						<?php $the_query = new WP_Query( array( 'post_type' => 'services', 'posts_per_page' => '4' ) ); ?>
+
+						<!-- Start The WP Loop -->
+						<?php while ( $the_query -> have_posts() ) : 
+								$the_query -> the_post(); ?>
+
+						       <!-- Display the post excerpt -->
+								<?php $post_display_option = get_theme_mod( 'post_display_option', 'post-excerpt' );
+								if ( 'post-excerpt' === $post_display_option ) {
+									get_template_part( 'template-parts/content', 'excerpt' );
+								} else {
+									get_template_part( 'template-parts/content', get_post_format() );
+								}
+								?>
+						<?php endwhile; wp_reset_query(); ?>
+
+					</section>
+
+
+			</div><!-- .entry-content -->
+
+		</div><!-- .wrap -->
+	</div><!-- .panel-content -->
+
+</article><!-- #post-<?php the_ID(); ?> -->
